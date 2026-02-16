335955d74e2a7bc2973921077fa93f88a50852e4
Islam Yamor <islamyamorz@gmail.com>
2026-02-16 12:42:52 +0300

    Made type test suite

--- tests/type.test.ts
+++ tests/type.test.ts
@@ -2,3 +2,7 @@ import { middlewareStack } from '../src'
 
 // @ts-expect-error - raw tuples not allowed
 middlewareStack([[/^\/test/, async () => {}]])
+
+test('type assertions are compile-time only', () => {
+  expect(true).toBe(true)
+})
