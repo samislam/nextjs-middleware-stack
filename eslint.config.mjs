dc027f1b5eaf92a5883b7810566b8797d1af3cc1
Islam Yamor <islamyamorz@gmail.com>
2026-02-16 12:43:55 +0300

    ignore eslint from tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,7 +3,7 @@ import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
-    ignores: ['dist/**'],
+    ignores: ['dist/**', 'tests/**'],
   },
   eslint.configs.recommended,
   tseslint.configs.recommended,
